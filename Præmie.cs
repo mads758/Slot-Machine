ca507315bb1b659dd8655228663175fe126ae9f8
And3r2 <73834785+And3r2@users.noreply.github.com>
2020-11-16 11:57:38 +0100

    Add files via upload

---
+++ Præmie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slot
+{
+    class Præmie
+    {
+        public void Win1()
+        {
+            Console.WriteLine("┌──────────────┬─────────────┬──────────────┬─────────┐");
+            Console.WriteLine("│ Første rulle │ Anden rulle │ Tredje rulle │ Gevinst │");
+            Console.WriteLine("├──────────────┼─────────────┼──────────────┼─────────┤");
+            Console.WriteLine("│       K      │             │              │     2   │");
+            Console.WriteLine("├──────────────┼─────────────┼──────────────┼─────────┤");
+            Console.WriteLine("│       K      │      K      │              │     5   │");
+            Console.WriteLine("├──────────────┼─────────────┼──────────────┼─────────┤");
+            Console.WriteLine("│       K      │      K      │      K       │    10   │");
+            Console.WriteLine("├──────────────┼─────────────┼──────────────┼─────────┤");
+            Console.WriteLine("│       A      │      A      │      A       │    15   │");
+            Console.WriteLine("├──────────────┼─────────────┼──────────────┼─────────┤");
+            Console.WriteLine("│       L      │      L      │      L       │    20   │");
+            Console.WriteLine("├──────────────┼─────────────┼──────────────┼─────────┤");
+            Console.WriteLine("│       B      │      B      │      B       │    50   │");
+            Console.WriteLine("├──────────────┼─────────────┼──────────────┼─────────┤");
+            Console.WriteLine("│       J      │      J      │      J       │   100   │");
+            Console.WriteLine("├──────────────┼─────────────┼──────────────┼─────────┤");
+            Console.WriteLine("│       P      │      P      │      P       │   500   │");
+            Console.WriteLine("└──────────────┴─────────────┴──────────────┴─────────┘");
+        }
+       
+
+    }
+}
